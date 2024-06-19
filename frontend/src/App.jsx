f3da9aea12f85f9026a6f6ea515b066395c9ecbb
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-19 20:19:01 +0530

    set up react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import {Button} from "@chakra-ui/button"
-function App() {
-  
+import { Button } from "@chakra-ui/button";
+import { Box, Container } from "@chakra-ui/react";
 
+function App() {
   return (
     <>
-     <Button>hello</Button>
+      <Container maxW="620px">
+      <Button>hello</Button>
+
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/theme-utils";
 import { ColorModeScript } from "@chakra-ui/color-mode";
 import { mode } from "@chakra-ui/theme-tools";
+import { BrowserRouter } from "react-router-dom";
 
 const styles = {
   global: (props) => ({
@@ -32,9 +33,12 @@ const theme = extendTheme({ config, styles, colors });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BrowserRouter>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
+    </BrowserRouter>
+
   </React.StrictMode>
 );
