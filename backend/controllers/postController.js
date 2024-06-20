3acf4b2fee5495edd10a5a1b4c6cf7410ceacf19
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-20 19:39:04 +0530

    add delete post function

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -54,6 +54,29 @@ const getPost = async (req, res) => {
 	}
 };
 
+const deletePost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		if (post.postedBy.toString() !== req.user._id.toString()) {
+			return res.status(401).json({ error: "Unauthorized to delete post" });
+		}
+
+		if (post.img) {
+			const imgId = post.img.split("/").pop().split(".")[0];
+			await cloudinary.uploader.destroy(imgId);
+		}
+
+		await Post.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ message: "Post deleted successfully" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
 
 
 
