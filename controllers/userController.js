c5fa769b549abe76af0d57457ef57d31ce112e07
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-20 18:43:18 +0530

    add logout controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,6 +98,19 @@ const loginUser = async (req, res) => {
 	}
 };
 
+const logoutUser = (req, res) => {
+	try {
+		res.cookie("jwt", "", { maxAge: 1 });
+		res.status(200).json({ message: "User logged out successfully" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log("Error in signupUser: ", err.message);
+	}
+};
+
+
+
+
 
 
 export {
