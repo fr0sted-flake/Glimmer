dd56f4fab0d3f0c8cc1d153af3631679b9e5315c
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-20 18:44:19 +0530

    user controller completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -223,7 +223,21 @@ const getSuggestedUsers = async (req, res) => {
 	}
 };
 
+const freezeAccount = async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id);
+		if (!user) {
+			return res.status(400).json({ error: "User not found" });
+		}
 
+		user.isFrozen = true;
+		await user.save();
+
+		res.status(200).json({ success: true });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
 
 export {
 	signupUser,
