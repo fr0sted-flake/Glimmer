ac1e5ac5acdc08e5a05aadca60504b0922fa272a
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-20 18:39:57 +0530

    add middlewares

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ dotenv.config();
 connectDB();
 const app= express()
 
+// Middlewares
+app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
