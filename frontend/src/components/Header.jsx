d0341c038cbbd0b3acc0297ae6679466e29e30b8
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-21 04:26:44 +0530

    add prettier

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,54 +10,62 @@ import authScreenAtom from "../atoms/authAtom";
 import { BsFillChatRightDotsFill } from "react-icons/bs";
 
 const Header = () => {
-	const { colorMode, toggleColorMode } = useColorMode();
-	const user = useRecoilValue(userAtom);
-	const logout = useLogout();
-	const setAuthScreen = useSetRecoilState(authScreenAtom);
+  const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
+  const logout = useLogout();
+  const setAuthScreen = useSetRecoilState(authScreenAtom);
 
-	return (
-		<Flex justifyContent={"space-between"} mt={6} mb='12'>
-			{user && (
-				<Link as={RouterLink} to='/'>
-					<AiFillHome size={24} />
-				</Link>
-			)}
-			{!user && (
-				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("login")}>
-					Login
-				</Link>
-			)}
+  return (
+    <Flex justifyContent={"space-between"} mt={6} mb="12">
+      {user && (
+        <Link as={RouterLink} to="/">
+          <AiFillHome size={24} />
+        </Link>
+      )}
+      {!user && (
+        <Link
+          as={RouterLink}
+          to={"/auth"}
+          onClick={() => setAuthScreen("login")}
+        >
+          Login
+        </Link>
+      )}
 
-			<Image
-				cursor={"pointer"}
-				alt='logo'
-				w={6}
-				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-				onClick={toggleColorMode}
-			/>
+      <Image
+        cursor={"pointer"}
+        alt="logo"
+        w={6}
+        src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+        onClick={toggleColorMode}
+      />
 
-			{user && (
-				<Flex alignItems={"center"} gap={4}>
-					<Link as={RouterLink} to={`/${user.username}`}>
-						<RxAvatar size={24} />
-					</Link>
-					<Link as={RouterLink} to={`/chat`}>
-						<BsFillChatRightDotsFill size={20} />
-					</Link>
-					
-					<Button size={"xs"} onClick={logout}>
-						<FiLogOut size={20} />
-					</Button>
-				</Flex>
-			)}
+      {user && (
+        <Flex alignItems={"center"} gap={4}>
+          <Link as={RouterLink} to={`/${user.username}`}>
+            <RxAvatar size={24} />
+          </Link>
+          <Link as={RouterLink} to={`/chat`}>
+            <BsFillChatRightDotsFill size={20} />
+          </Link>
 
-			{!user && (
-				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("signup")}>
-					Sign up
-				</Link>
-			)}
-		</Flex>
-	);
+          <Button size={"xs"} onClick={logout}>
+            <FiLogOut size={20} />
+          </Button>
+        </Flex>
+      )}
+
+      {!user && (
+        <Link
+          as={RouterLink}
+          to={"/auth"}
+          onClick={() => setAuthScreen("signup")}
+        >
+          Sign up
+        </Link>
+      )}
+    </Flex>
+  );
 };
 
 export default Header;
