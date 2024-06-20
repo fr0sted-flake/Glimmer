c0a70b713e8e324423884f8ef0398a3ed32bbe7b
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-20 19:38:51 +0530

    get post controller added

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -40,6 +40,20 @@ const createPost = async (req, res) => {
 	}
 };
 
+const getPost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
 
 
 
