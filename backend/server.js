d44a9340ccf437e2143f709d4a8e19b934856b8a
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-20 18:40:17 +0530

    add routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
 
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/messages", messageRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 
